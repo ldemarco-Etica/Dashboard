6c780d21b46a085bd67a108917fe336d9cd09742
ldemarco-Etica <ldemarco@eticasgr.it>
2026-01-15 13:20:10 +0100

    Update scheduler_restart.py

--- scheduler_restart.py
+++ scheduler_restart.py
@@ -11,7 +11,7 @@ def restart_app():
 
 # Schedula riavvii alle 7:30 e 17:30
 schedule.every().day.at("07:30").do(restart_app)
-schedule.every().day.at("13:25").do(restart_app)
+schedule.every().day.at("13:21").do(restart_app)
 schedule.every().day.at("17:30").do(restart_app)
 
 print("⏰ Scheduler attivo - riavvii programmati alle 7:30 e 17:30")
